66b414b5984ef42e41b1c227ce9b65296c3174cf
zoidberg <vicarious@mailbox.org>
2020-05-30 00:50:01 +0200

    init

---
+++ content/pages/about.md
@@ -0,0 +1,16 @@
+Title: About
+Slug:
+Date: 20.05.2020
+Category: Personal
+Tags: @me
+Author: 0x00
+
+# About the Blog and me
+
+**This blog is currently in the alpha phase**
+
+This is complete personal blog that operates as some kind of journal for me. Thus I want to share solutions for security challenges like writeups. Report about interesting papers, books or articles I just read. Maybe sharing some insights on topics from university. So I would be some kind of mixed, about most topics will have references to computer security.
+
+I rather think there are no relevant or interesting things to say about me (as I assume no one will really read this blog at all). But for completeness sake: I'm a computer science student placed in Berlin (Free University Berlin) with a focus in computer security and operating systems.
+
+For more information I will upload a CV, just in case there are people who are interested in (Google-search bot I'm looking at you :) ).
---
+++ pelicanconf.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = '0x00'
+SITENAME = '0x00s blog'
+SITEURL = ''
+
+PATH = 'content'
+STATIC_PATHS = ['pdfs']
+
+TIMEZONE = 'Europe/Berlin'
+
+DEFAULT_LANG = 'de'
+
+MARKUP = ('md', 'ipynb')
+
+PLUGIN_PATHS = ['./plugins', 'pelican-plugins']
+PLUGINS = ['ipynb.markup', 'render_math', 'jinja2content', 'tag_cloud', 'i18n_subsites']
+JINJA_ENVIRONMENT = {
+    'extensions': ['jinja2.ext.i18n'],
+}
+
+THEME = 'pelican-bootstrap3'
+
+#JINJA_EXTENSIONS = ['jinja2.ext.i18n']
+
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Content licensing: CC-BY
+CC_LICENSE = "CC-BY"
+
+# LINKS
+#LINKS = (('Project Euler', 'https://projecteuler.net/'), ('Hackerrank', 'https://www.hackerrank.com/'),)
+
+# Social widget
+SOCIAL = (('@github', 'https://github.com/sh3llc0d3r'),
+            ('@twitter', '@0xdeadbeef6'),
+          ('z01db3rg@gitlab.com', 'https://gitlab.com/z01db3rg'))
+
+DEFAULT_PAGINATION = 10
+
+IGNORE_FILES = ['.ipynb_checkpoints']
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
